e45c600fe7053673cea3a3428b87f9268f0f78e8
Fakhirazzahrah Ramadhani <fzzahrahh@gmail.com>
2024-05-08 00:58:03 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const init = async() => {
     const server = Hapi.server({
         port: 5000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes:{
-            cors: {
-                origin: ['*'],
-            },
+        routes: {
+          cors: {
+            origin: ['*'],
+          },
         },
-    });
+      });
 
     await server.route(routes);
 
